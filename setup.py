08e14759a63590e94abe786b94de02ff4ba52418
StefansArya <stefansarya1@gmail.com>
2023-03-18 18:50:17 +0700

    Update to v0.8.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='Blackprint',
-	version='0.8.13',
+	version='0.8.14',
 	packages=['Blackprint'],
 	# scripts=['bin/script1','bin/script2'],
 	url='https://github.com/Blackprint/engine-python',
