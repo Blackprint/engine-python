c9764cf1d1ec202d7eb4114731a687c1576d4b66
StefansArya <stefansarya1@gmail.com>
2025-10-18 13:09:21 +0700

    Add different color for different node log

--- example/BPNode/Example/utils.py
+++ example/BPNode/Example/utils.py
@@ -1,5 +1,29 @@
 import os
 os.system('color')
 
-def colorLog(bright, dark=''):
-	print("\x1b[1m\x1b[33m" + bright + "\x1b[0m \x1b[33m" + str(dark) + "\x1b[0m")
+def colorLog(category, message=''):
+	# Color definitions
+	colors = {
+		'reset': "\033[0m",
+		'red': "\033[31m",
+		'green': "\033[32m",
+		'yellow': "\033[33m",
+		'blue': "\033[34m",
+		'magenta': "\033[35m",
+		'cyan': "\033[36m",
+		'white': "\033[37m",
+	}
+
+	# Determine category color
+	category_color = colors['cyan']
+	if 'Button' in category:
+		category_color = colors['blue']
+	elif 'Logger' in category:
+		category_color = colors['green']
+	elif 'Input' in category:
+		category_color = colors['yellow']
+	elif 'Math' in category:
+		category_color = colors['magenta']
+
+	# Print formatted message
+	print(f"{category_color}{category}{colors['reset']} {colors['white']}{message}{colors['reset']}")
